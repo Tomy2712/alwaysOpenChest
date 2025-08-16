cd42dc812205d99725bde21e560b222c1ab6a49b
Tomy2712 <tomrousee@gmail.com>
2025-08-16 15:20:18 +0200

    Compatibility with iron chest mod

---
+++ src/main/java/fr/tomy2712/alwaysopenchest/mixins/AbstractIronChestBlockMixin.java
@@ -0,0 +1,24 @@
+package fr.tomy2712.alwaysopenchest.mixins;
+
+import net.minecraft.core.BlockPos;
+
+import net.minecraft.world.level.LevelAccessor;
+import org.spongepowered.asm.mixin.Mixin;
+import com.progwml6.ironchest.common.block.regular.AbstractIronChestBlock;
+import org.spongepowered.asm.mixin.Overwrite;
+
+
+@Mixin(AbstractIronChestBlock.class)
+public abstract class AbstractIronChestBlockMixin {
+
+    /**
+     * @author Tomy2712
+     * @reason Always open chest from iron chest
+     * @return Always false to prevent blocking the chest
+     */
+    @Overwrite(remap = false)
+    public static boolean isChestBlockedAt(LevelAccessor levelAccessor, BlockPos blockPos) {
+        return false;
+    }
+}
+
